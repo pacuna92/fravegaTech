a66ac64b98bc136ddb543e31bb34adbbcbfafb2d
pacuna92 <pablojoaquinacuna@gmail.com>
2024-12-20 20:47:01 -0300

    Enter zip code in popup

---
+++ tests/e2e/searchProduct.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect, chromium, Browser, BrowserContext, Page } from '@playwright/test';
+import ZipCodePopUp from '../pages/ZipCode';
+import dotenv from 'dotenv';
+import { ZIP_CODES } from '../fixtures/zipCode'; 
+dotenv.config();
+
+test('Search product and add to cart if available in stock', async ({ page }) => {
+  
+  const zipCode = new ZipCodePopUp(page);
+
+  await page.goto('https://www.fravega.com/');
+  
+  await zipCode.enterPostalCode(ZIP_CODES[0].zipCode);
+  
+});
+
